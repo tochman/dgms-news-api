922736a150afe20fd65cc19ed2bf04c328585498
shagufta <shagufta.batool8@gmail.com>
2022-03-23 23:55:06 +0100

    configure rack-cors for cross origin resoure sharing

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,16 @@ module DgmsNewsAdmin
   class Application < Rails::Application
     config.load_defaults 6.1
     config.api_only = true
-
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+          headers: :any,
+          methods: %i[get post put delete],
+          expose: %w(access-token expiry token-type uid client),
+          max_age: 0
+      end
+    end
     config.generators do |generate|
       generate.helper false
       generate.assets false
