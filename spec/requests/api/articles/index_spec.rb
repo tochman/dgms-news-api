3c82bc618be4d32c4bcfc1e2ae20e04b32b8feaa
maxarvid <max.arvid.anderson@gmail.com>
2022-04-07 10:37:28 +0200

    🔥 removes comments

--- spec/requests/api/articles/index_spec.rb
+++ spec/requests/api/articles/index_spec.rb
@@ -12,10 +12,6 @@ RSpec.describe 'GET /api/articles', type: :request do
 
   it { is_expected.to have_http_status 200 }
 
-  # it 'is expected to return a collection of 5 articles' do
-  #   expect(response_json['articles'].size).to eq 5
-  # end
-
   it 'is expected to return articles grouped by category' do
     expect(response_json['articles'].keys).to match ['sports', 'business']
   end
