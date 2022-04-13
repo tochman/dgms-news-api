7f75d5a96d2d0a260c20133204b8dbb949eacb59
maxarvid <max.arvid.anderson@gmail.com>
2022-04-13 10:52:34 +0200

    Adds authentication for articles show and create actions

--- spec/requests/api/articles/create_spec.rb
+++ spec/requests/api/articles/create_spec.rb
@@ -1,133 +1,114 @@
-RSpec.describe "POST /api/articles" do
-
+RSpec.describe 'POST /api/articles' do
   subject { response }
 
-  let!(:sports_category) { create(:category, name: "Sports") }
-  let!(:business_category) { create(:category, name: "Business") }
-
-
-
-
+  let(:user) { create(:user) }
+  let(:credentials) { user.create_new_auth_token }
+  let!(:sports_category) { create(:category, name: 'Sports') }
+  let!(:business_category) { create(:category, name: 'Business') }
 
   describe 'as an authenticated user' do
-
-
-    describe "successfully" do
-
+    describe 'successfully' do
       before do
-        post "/api/articles", params: {
-                                article: { title: "News about Sports", body: "Lorem ipsum...", category: "Sports" },
-                              }
+        post '/api/articles', params: {
+          article: { title: 'News about Sports', body: 'Lorem ipsum...', category: 'Sports' }
+        }, headers: credentials
 
         @article = Article.last
       end
 
       it { is_expected.to have_http_status 201 }
-      #it { is_expected.to have_http_status :created }
 
-      it "is expected to create an instance of an Article" do
+      it 'is expected to create an instance of an Article' do
         expect(@article).not_to be nil
       end
 
-      it "is expected to have a title" do
-        expect(@article.title).to eq "News about Sports"
+      it 'is expected to have a title' do
+        expect(@article.title).to eq 'News about Sports'
       end
 
-      it "is expected to have a body" do
-        expect(@article.body).to eq "Lorem ipsum..."
+      it 'is expected to have a body' do
+        expect(@article.body).to eq 'Lorem ipsum...'
       end
 
-      it "is expected to respond with a confirmation message" do
-        expect(response_json["message"]).to eq "Article created successfully"
+      it 'is expected to respond with a confirmation message' do
+        expect(response_json['message']).to eq 'Article created successfully'
       end
-      
     end
 
-
-    describe "unsuccessfully" do
-    
-      describe "due to missing params" do
-
+    describe 'unsuccessfully' do
+      describe 'due to missing params' do
         before do
-          post "/api/articles", params: {}
+          post '/api/articles', params: {}, headers: credentials
         end
 
         it { is_expected.to have_http_status :unprocessable_entity }
 
-        it "is expected to respond with an error message" do
-          expect(response_json["message"]).to eq "Missing params"
+        it 'is expected to respond with an error message' do
+          expect(response_json['message']).to eq 'Missing params'
         end
-
       end
 
-      describe "due to missing title" do
-
+      describe 'due to missing title' do
         before do
-          post "/api/articles", params: {
-                                  article: {
-                                    body: "Lorem ipsum...",
-                                    category: "Sports"
-                                  },
-                                }
+          post '/api/articles', params: {
+            article: {
+              body: 'Lorem ipsum...',
+              category: 'Sports'
+            }
+          }, headers: credentials
         end
 
         it { is_expected.to have_http_status :unprocessable_entity }
 
-        it "is expected to respond with an error message" do
-          expect(response_json["message"]).to eq "Title can't be blank"
+        it 'is expected to respond with an error message' do
+          expect(response_json['message']).to eq "Title can't be blank"
         end
-
       end
 
-      describe "due to missing body" do
-
+      describe 'due to missing body' do
         before do
-          post "/api/articles", params: {
-                                  article: {
-                                    title: "News about Sports",
-                                    category: "Sports"
-                                  },
-                                }
+          post '/api/articles', params: {
+            article: {
+              title: 'News about Sports',
+              category: 'Sports'
+            }
+          }, headers: credentials
         end
 
         it { is_expected.to have_http_status :unprocessable_entity }
 
-        it "is expected to respond with an error message" do
-          expect(response_json["message"]).to eq "Body can't be blank"
+        it 'is expected to respond with an error message' do
+          expect(response_json['message']).to eq "Body can't be blank"
         end
-
       end
 
-      describe "due to missing category" do
-
+      describe 'due to missing category' do
         before do
-          post "/api/articles", params: {
-                            article: { title: "News about Sports", body: "Lorem ipsum..." },
-                          }
+          post '/api/articles', params: {
+            article: { title: 'News about Sports', body: 'Lorem ipsum...' }
+          }, headers: credentials
         end
 
         it { is_expected.to have_http_status :unprocessable_entity }
 
-        it "is expected to respond with an error message" do
-          expect(response_json["message"]).to eq "Category can't be blank"
+        it 'is expected to respond with an error message' do
+          expect(response_json['message']).to eq "Category can't be blank"
         end
-
       end
-
     end
-
-
   end
-
-
-
 
   describe 'as an non-authenticated user' do
+    before do
+      post '/api/articles', params: {
+        article: { title: 'News about Sports', body: 'Lorem ipsum...', category: 'Sports' }, headers: nil
+      }
+    end
 
+    it { is_expected.to have_http_status 401 }
+
+    it 'is expected to respond with an error message' do
+      expect(response_json['errors'].first).to eq 'You need to sign in or sign up before continuing.'
+    end
   end
-
-
-
-
-
 end
--- spec/requests/api/articles/show_spec.rb
+++ spec/requests/api/articles/show_spec.rb
@@ -1,26 +1,42 @@
 RSpec.describe 'GET /api/articles/:id', type: :request do
-
+  let(:user) { create(:user) }
+  let(:credentials) { user.create_new_auth_token }
   let(:article) { create(:article, title: 'Article 1') }
   let!(:another_article) { create(:article, title: 'Article 2') }
 
-  before do
-    get "/api/articles/#{article.id}"
-  end
-
   subject { response }
 
-  it { is_expected.to have_http_status 200 }
+  describe 'as an authenticated user' do
+    describe 'successfully' do
+      before do
+        get "/api/articles/#{article.id}", headers: credentials
+      end
 
-  it 'is expected to return 1 article' do
-    expect(response_json.size).to eq 1
+      it { is_expected.to have_http_status 200 }
+
+      it 'is expected to return 1 article' do
+        expect(response_json.size).to eq 1
+      end
+
+      it 'is expected to include article title' do
+        expect(response_json['article']['title']).to eq 'Article 1'
+      end
+
+      it 'is expected to include the right article title' do
+        expect(response_json['article']['title']).not_to eq 'Article 2'
+      end
+    end
   end
 
-  it 'is expected to include article title' do
-    expect(response_json['article']['title']).to eq 'Article 1'
-  end
+  describe 'as an unauthenticated user' do
+    before do
+      get "/api/articles/#{article.id}", headers: nil
+    end
 
-  it 'is expected to include the right article title' do
-    expect(response_json['article']['title']).not_to eq 'Article 2'
-  end
+    it { is_expected.to have_http_status 401 }
 
+    it 'is expected to respond with an error message' do
+      expect(response_json['errors'].first).to eq 'You need to sign in or sign up before continuing.'
+    end
+  end
 end
